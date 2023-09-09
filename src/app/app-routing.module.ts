283053a607d3168ffad5119a56b03502289a08c4
hidekits13 <hidekits13@gmail.com>
2023-09-08 22:48:14 -0500

    Avance de Front-end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,51 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'menu',
+    loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule)
+  },
+  {
+    path: 'menu-carrera',
+    loadChildren: () => import('./pages/menu-carrera/menu-carrera.module').then( m => m.MenuCarreraPageModule)
+  },
+  {
+    path: 'sistemas',
+    loadChildren: () => import('./pages/sistemas/sistemas.module').then( m => m.SistemasPageModule)
+  },
+  {
+    path: 'civil',
+    loadChildren: () => import('./pages/civil/civil.module').then( m => m.CivilPageModule)
+  },
+  {
+    path: 'industrial',
+    loadChildren: () => import('./pages/industrial/industrial.module').then( m => m.IndustrialPageModule)
+  },
+  {
+    path: 'aeronautica',
+    loadChildren: () => import('./pages/aeronautica/aeronautica.module').then( m => m.AeronauticaPageModule)
+  },
+  {
+    path: 'arquitectura',
+    loadChildren: () => import('./pages/arquitectura/arquitectura.module').then( m => m.ArquitecturaPageModule)
+  },
+  {
+    path: 'oficinas',
+    loadChildren: () => import('./pages/oficinas/oficinas.module').then( m => m.OficinasPageModule)
+  },
+
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
